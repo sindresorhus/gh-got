df53dbec3c6f140557bc4a3b8969371fc031c622
Vsevolod Strukchinsky <floatdrop@gmail.com>
2015-10-23 11:00:03 +0500

    Add streaming interface
    Closes #3

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
 import test from 'ava';
+import getStream from 'get-stream';
 import fn from './';
 
 test('default', async t => {
@@ -17,4 +18,9 @@ test('token option', t => {
 test('endpoint option', t => {
 	// TODO: use `t.throws` when it supports promises
 	return fn('users/sindresorhus', {endpoint: 'fail'}).catch(err => t.ok(err));
+});
+
+test('stream interface', async t => {
+	t.is(JSON.parse(await getStream(fn.stream('users/sindresorhus'))).login, 'sindresorhus');
+	t.is(JSON.parse(await getStream(fn.stream.get('users/sindresorhus'))).login, 'sindresorhus');
 });
