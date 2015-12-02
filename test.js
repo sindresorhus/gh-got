c536245c8f7a24a5a911eb5380120fa9b1c84ced
Sam Verschueren <sam.verschueren@gmail.com>
2015-12-02 21:52:41 +0100

    tweaks

--- test.js
+++ test.js
@@ -2,6 +2,10 @@ import test from 'ava';
 import getStream from 'get-stream';
 import fn from './';
 
+global.Promise = Promise;
+
+const token = process.env.GITHUB_TOKEN;
+
 test('default', async t => {
 	t.is((await fn('users/sindresorhus')).body.login, 'sindresorhus');
 });
@@ -10,14 +14,14 @@ test('should accept options', async t => {
 	t.is((await fn('users/sindresorhus', {})).body.login, 'sindresorhus');
 });
 
-test('token option', t => {
-	// TODO: use `t.throws` when it supports promises
-	return fn('users/sindresorhus', {token: 'fail'}).catch(err => t.ok(err));
+test.serial('token option', async t => {
+	process.env.GITHUB_TOKEN = 'fail';
+	await t.throws(fn('users/sindresorhus'), 'Response code 401 (Unauthorized)');
+	process.env.GITHUB_TOKEN = token;
 });
 
-test('endpoint option', t => {
-	// TODO: use `t.throws` when it supports promises
-	return fn('users/sindresorhus', {endpoint: 'fail'}).catch(err => t.ok(err));
+test('endpoint option', async t => {
+	await t.throws(fn('users/sindresorhus', {endpoint: 'fail', retries: 1}), /ENOTFOUND/);
 });
 
 test('stream interface', async t => {
